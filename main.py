bd188fec32e4bc57fa323c6cd0e4489c86f9856f
lawrence <prostoLavr@mail.ru>
2021-10-10 11:09:56 +0800

    structure reformat

--- main.py
+++ main.py
@@ -7,21 +7,40 @@ from my_widgets.task_widget import Ui_Frame as Ui_TaskWidget
 import sys
 
 
-class MainWindow(QMainWindow, Ui_MainWindow):
-    def __init__(self):
-        super().__init__()
+class SetupMainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.setupUi(self)
         self.my_setupUi()
-        self.add_task()
 
     def my_setupUi(self):
         self.home_widget_obj = HomeWidget(self.add_widget)
         self.statistic_widget_obj = StatisticWidget(self.statictic_widget)
-        self.buttons_connect()
-        self.close_all()
         self.show()
 
-    def buttons_connect(self):
+    def resizeEvent(self, a0: QtGui.QResizeEvent) -> None:
+        width, height = a0.size().width(), a0.size().height()
+        hp = height / 100
+        # wp = width / 100
+        self.centralwidget.setGeometry(0, 0, width, height)
+        # self.homeButton.setFixedHeight(round(90 * hp))
+        self.settingsButton.setMaximumHeight(round(10*hp))
+        self.horizontalWidget.setGeometry(0, 0, width, height)
+        # self.leftWidget.setGeometry(0, 0, 50*wp, height)
+
+    # def add_task(self):
+    #     new_task = TaskWidget('hello', 15, 5, self.statictic_widget_obj)
+    #     self.statictic_widget_obj.my_layout.addStretch(1)
+    #     self.statictic_widget_obj.my_layout.addWidget(new_task)
+
+
+class MenuConnectMainWindow(SetupMainWindow):
+    def __init__(self, *args, **kwargs):
+        super(MenuConnectMainWindow, self).__init__(*args, **kwargs)
+        self.menu_buttons_connect()
+        self.close_all()
+
+    def menu_buttons_connect(self):
         self.menu_buttons = (self.homeButton, self.historyButton, self.statisticButton, self.settingsButton)
         for button in self.menu_buttons:
             button.clicked.connect(self.menu_button_click)
@@ -50,23 +69,17 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         for button in self.menu_buttons:
             button.setStyleSheet('')
         self.sender().setStyleSheet('background: "#AAA";')
-        print(f'open {self.sender().text()}')
+        print(f'Открыта вкладка {self.sender().text()}')
         self.buttons_action_dct[self.sender().text()]()
 
-    def resizeEvent(self, a0: QtGui.QResizeEvent) -> None:
-        width, height = a0.size().width(), a0.size().height()
-        hp = height / 100
-        # wp = width / 100
-        self.centralwidget.setGeometry(0, 0, width, height)
-        # self.homeButton.setFixedHeight(round(90 * hp))
-        self.settingsButton.setMaximumHeight(round(10*hp))
-        self.horizontalWidget.setGeometry(0, 0, width, height)
-        # self.leftWidget.setGeometry(0, 0, 50*wp, height)
 
-    def add_task(self):
-        new_task = TaskWidget('hello', 15, 5, self.statictic_widget_obj)
-        self.statictic_widget_obj.my_layout.addStretch(1)
-        self.statictic_widget_obj.my_layout.addWidget(new_task)
+class ButtonConnectMainWindow(MenuConnectMainWindow):
+    def __init__(self, *args, **kwargs):
+        super(ButtonConnectMainWindow, self).__init__(*args, **kwargs)
+        self.connect_button()
+
+    def connect_button(self):
+        pass
 
 
 class HomeWidget(Ui_Frame, QWidget):
@@ -85,23 +98,24 @@ class StatisticWidget(QScrollArea):
 class TaskWidget(Ui_TaskWidget, QFrame):
     def __init__(self, text, time_start, long_time,  parent):
         super(TaskWidget, self).__init__()
-        self.set_text(text)
-        self.set_times(time_start, long_time)
+        self.text = self.set_text(text)
+        self.time_start, self.duration, self.time_end = self.set_times(time_start, long_time)
         self.setupUi(parent)
 
     def text_for_label(self):
         return '|'.join(map(str, (self.text, self.time_start, self.long_time)))
 
-    def set_text(self, text):
-        self.text = text
+    @staticmethod
+    def set_text(text):
+        return text
 
-    def set_times(self, time_start, long_time):
-        self.time_start = time_start
-        self.long_time = long_time
-        self.end_time = time_start + long_time
+    @staticmethod
+    def set_times(time_start, duration):
+        time_end = time_start + duration
+        return time_start, time_end, duration
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = MainWindow()
+    ex = ButtonConnectMainWindow()
     sys.exit(app.exec_())
